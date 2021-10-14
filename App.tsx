05b8c6d75d6b80c9344c27a04ebba77b38248d05
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-14 23:36:18 +0000

    update

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Screens } from "./navigations";
+import { Screens, NativeStackParamList } from "./navigations";
 import { Login } from "./screens";
 import { Home } from "./screens/Home";
 import { Cart } from "./screens/Cart";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<NativeStackParamList>();
 export default function App() {
   return (
     <NavigationContainer>
