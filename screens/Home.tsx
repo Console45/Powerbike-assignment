fc66dc25a1ba79d6905cf2a0661331cee1fdd17c
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-15 13:29:58 +0000

    added caption and tabs

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,7 @@
 import React, { FC, ReactElement } from "react";
 import { View, StyleSheet, SafeAreaView, Text } from "react-native";
-import { HomeHeader } from "../layouts";
-import { Colors } from "../theme";
+import { HomeHeader, Tabs } from "../layouts";
+import { Colors, sizes } from "../theme";
 
 interface HomeProps {}
 
@@ -9,6 +9,20 @@ export const Home: FC<HomeProps> = (): ReactElement => {
   return (
     <SafeAreaView style={styles.container}>
       <HomeHeader />
+      <View style={styles.captionContainer}>
+        <Text style={{ color: Colors.GRAY, fontSize: 17 }}>The World's </Text>
+        <Text
+          style={{
+            color: Colors.PRIMARY,
+            fontSize: 23,
+            fontWeight: "bold",
+            marginBottom: -1,
+          }}
+        >
+          Best Bike
+        </Text>
+      </View>
+      <Tabs />
     </SafeAreaView>
   );
 };
@@ -17,5 +31,11 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.WHITE,
     flex: 1,
+  },
+  captionContainer: {
+    flexDirection: "row",
+    paddingHorizontal: sizes.mainPadding,
+    marginTop: 20,
+    alignItems: "flex-end",
   },
 });
