6648e048258d3e99a4432aae08cc8c8853dc9401
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-15 13:28:25 +0000

    added tabs

---
+++ layouts/Tabs.tsx
@@ -0,0 +1,55 @@
+import React, { FC, ReactElement, Key } from "react";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import { Colors, sizes } from "../theme";
+
+interface TabsProps {}
+
+export const Tabs: FC<TabsProps> = ({}): ReactElement => {
+  const categories: string[] = ["All", "Road bike", "Mountain", "Urban bike"];
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontWeight: "bold", fontSize: 17 }}>Categories</Text>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        style={{ width: "100%", marginTop: 15 }}
+      >
+        {categories.map((category: string, index: number) => {
+          return <TabItem title={category} index={index} key={index} />;
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+interface TabItemProps {
+  title: string;
+  index?: number;
+}
+const TabItem: FC<TabItemProps> = ({ title, index }): ReactElement => {
+  const active = index === 0 ? styles.active : styles.text;
+  return (
+    <View style={styles.tabItem}>
+      <Text style={active}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: sizes.mainPadding,
+    marginTop: 23,
+  },
+  tabItem: {
+    backgroundColor: Colors.SECONDARY,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 12,
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    borderRadius: 12,
+  },
+  text: { color: Colors.GRAY, fontWeight: "500" },
+  active: { color: Colors.PRIMARY, fontWeight: "700" },
+});
