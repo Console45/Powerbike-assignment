d8594faba42df19a6ee67cecef8e492a4afcf59b
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-15 01:40:51 +0000

    updated colors

--- theme/colors.ts
+++ theme/colors.ts
@@ -1,8 +1,9 @@
 export enum Colors {
   PRIMARY = "#F16905",
-  SECONDARY = "#f5f5f5",
-  TERTIARY = "#1A1A1A",
+  SECONDARY = "#f5f5f6",
+  TERTIARY = "#151110",
   WHITE = "#ffffff",
   BLACK = "#000000",
-  GRAY = "#ADADAD",
+  GRAY = "#A4a3a3",
+  DEEP_GRAY = "#6f7f88",
 }
